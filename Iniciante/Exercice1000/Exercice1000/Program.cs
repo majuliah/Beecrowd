3867871f8370c14106dc2663307d1f528adf972e
majulha <majulialcarvalho@gmail.com>
2023-02-09 20:21:10 -0300

    inserting 1003 - 1008

--- Iniciante/Exercice1000/Exercice1000/Program.cs
+++ Iniciante/Exercice1000/Exercice1000/Program.cs
@@ -23,8 +23,52 @@ namespace Exercice1000
             WriteLine($"Area = {Exercice1002(radios).ToString($"F4", CultureInfo.InvariantCulture)}");*/
             
             //exercice 1003
+            /*WriteLine($"Enter with two numbers: ");
+            int numberOne = int.Parse(ReadLine());
+            int numberTwo = int.Parse(ReadLine());
+            WriteLine($"SOMA = {Exercice1003(numberOne, numberTwo)}");
+            */
             
-
+            //exercice 1004
+            /*
+            WriteLine($"Enter with two numbers: ");
+            double numberOne = double.Parse(ReadLine(), CultureInfo.InvariantCulture);
+            double numberTwo = double.Parse(ReadLine(), CultureInfo.InvariantCulture));
+            WriteLine($"PRODUTO = {Exercice1004(numberOne, numberTwo)}");
+            */
+            
+            //exercice 1005
+            /*WriteLine($"Enter with two numbers: ");
+            double numberOne = double.Parse(ReadLine(), CultureInfo.InvariantCulture);
+            double numberTwo = double.Parse(ReadLine(), CultureInfo.InvariantCulture);
+            WriteLine($"MEDIA = {Exercice1005(numberOne, numberTwo)}");     
+            */      
+            
+            //exercice 1006
+            /*WriteLine($"Enter with three numbers: ");
+            double numberOne = double.Parse(ReadLine(), CultureInfo.InvariantCulture);
+            double numberTwo = double.Parse(ReadLine(), CultureInfo.InvariantCulture);
+            double numberThree = double.Parse(ReadLine(), CultureInfo.InvariantCulture);
+            WriteLine($"MEDIA = {Exercice1006(numberOne, numberTwo, numberThree).ToString($"F2", CultureInfo.InvariantCulture)}");
+            */
+            
+            //exercice1007
+            /*WriteLine($"Enter with four numbers: ");
+            int numberOne = int.Parse(ReadLine());
+            int numberTwo = int.Parse(ReadLine());
+            int numberThree = int.Parse(ReadLine());
+            int numberFour = int.Parse(ReadLine());
+            WriteLine($"DIFERENÇA = {Exercice1007(numberOne, numberTwo, numberThree, numberFour)}");
+            */
+            
+            //exercice 1008
+            WriteLine($"Enter with an ID, a total of hours and the value hour: ");
+            int id = int.Parse(ReadLine());
+            double hours = double.Parse(ReadLine(), CultureInfo.InvariantCulture);
+            double valueHour = double.Parse(ReadLine(), CultureInfo.InvariantCulture);
+            WriteLine($"NUMBER = {id}" +
+                      $"SALARY = {Exercice1008(id, hours, valueHour)}");
+                
         }
         public static string Message()
         {
@@ -37,12 +81,68 @@ namespace Exercice1000
             int result = numberOne + numberTwo;
             return result;
         }
-        
         public static double Exercice1002(double radios)
         {
             double pi = 3.14159;
             double area = pi * (Math.Pow(radios, 2.0));
             return area;
         }
+        public static int Exercice1003(int number1, int number2)
+        {
+            int sum = 0;
+            
+            if (number1 == 0 && number2 == 0)
+                WriteLine($"You have to give at least one number different than 0. ");
+            else
+                sum = number1 + number2;
+            
+            return sum;
+        }
+        public static double Exercice1004(double number1, double number2)
+        {
+            double product = 0.0;
+
+            if (number1 == 0 && number2 == 0)
+                WriteLine($"You have to give at least one number different than 0. ");
+            else
+                product = number1 * number2;
+            
+            return product;
+        }
+        public static double Exercice1005(double number1, double number2)
+        {
+            double media = 0.0;
+
+            if (number1 == 0 && number2 == 0)
+                WriteLine($"You have to give at least one number different than 0. ");
+            else
+                media = number1 * 3.5 + number2 * 7.5 / 11.0;
+            
+            return media;
+        }        
+        public static double Exercice1006(double number1, double number2, double number3)
+        {
+            double media = 0.0;
+
+            if (number1 == 0.0 && number2 == 0.0 && number3 == 0.0)
+                WriteLine($"You have to give at least one number different than 0. ");
+            else
+                media = (number1 * 2.0 + number2 * 3.0 + number3 * 5.0) / 10.0;
+            
+            return media;
+        }
+        public static int Exercice1007(int number1, int number2, int number3, int number4)
+        {
+            int diferenca = number1 * number2 - number3 * number4;
+            return diferenca;
+        }
+
+        public static double Exercice1008(int number, double hour, double valueHour)
+        {
+            double totalSalary = hour * valueHour;
+            return totalSalary;
+        }
+        
+       public static void Exercice1009(){}
     }
 }
