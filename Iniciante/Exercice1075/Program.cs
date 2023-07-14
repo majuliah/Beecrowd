bed64b09dd86cb14b75360ed22147ba0f0b812a5
majulha <majulialcarvalho@gmail.com>
2023-07-14 14:49:37 -0300

    Exercicio 1078

--- Iniciante/Exercice1075/Program.cs
+++ Iniciante/Exercice1075/Program.cs
@@ -7,7 +7,10 @@ namespace Exercice1075
         static void Main(string[] args)
         {
             //Exercice1075
-            Exercice1075();
+            //Exercice1075();
+            
+            //Exercice1078
+            //Exercice1078();
         }
 
         static void Exercice1075()
@@ -21,6 +24,18 @@ namespace Exercice1075
                     WriteLine(i);
             }
             
+        }
+        static void Exercice1078()
+        {
+            short number = 0;
+            do
+            {
+                WriteLine("Enter with a number");
+                number = short.Parse(ReadLine());
+            } while (number < 2 & number >= 1000);
+
+            for (int i = 1; i <= 10; i++)
+                WriteLine($"{i} x {number} = {i * number}");
         }
     }
 }
