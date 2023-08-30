f8b74086760fcbf76ed3d26d8e4b30b755d9f096
majulha <majulialcarvalho@gmail.com>
2023-08-30 15:55:48 -0300

    exercice 1153

--- Iniciante/Exercice1145/Program.cs
+++ Iniciante/Exercice1145/Program.cs
@@ -12,8 +12,31 @@ namespace Exercice1145
             
             //Exercice1146
             //Exercice1146();
+            
+            //Exercice1149
+            //Exercice1149();
+            
+            //Exercice1153
+            //Exercice1153();
+            
         }
+        static void Exercice1153()
+        {
+            int fatorial = 0;
+            int numero = int.Parse(ReadLine());
 
+            for (int i = 1; i <= numero; i++)
+                fatorial += numero * (numero - i);
+            
+            WriteLine(fatorial);
+        }
+        static void Exercice1149()
+        {
+            int A = 0;
+            int N = 0;
+            A = int.Parse(ReadLine());
+            //entender esta questão e resolver!
+        }
         static void Exercice1146()
         {
             int numero = 1;
