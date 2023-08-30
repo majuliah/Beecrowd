c0545682673ddadd3e50fbc802a44b6bd8813456
majulha <majulialcarvalho@gmail.com>
2023-08-30 14:38:04 -0300

    exercicio 1145

---
+++ Iniciante/Exercice1145/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using static System.Console;
+
+namespace Exercice1145
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Exercice1145
+            Exercice1145();
+        }
+
+        static void Exercice1145()
+        {
+            string[] numeros = ReadLine().Split(' ');
+            int numerosPorLinha = int.Parse(numeros[0]);
+            int final = int.Parse(numeros[1]);
+            int contador = 1;
+
+            do
+            {
+                for (int i = 1; i <= numerosPorLinha; i++)
+                {
+                    Write($"{contador} ");
+                    contador++;
+                }
+                WriteLine();
+            } while (contador <= final);
+        }
+    }
+}
