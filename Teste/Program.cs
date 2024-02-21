b59b3efe5ed6364853526f2e9a85b6bcd82d5b58
Majulia <majulialcarvalho@gmail.com>
2024-02-20 21:57:18 -0300

    exercício para testar ide

---
+++ Teste/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Teste
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Leia um valor inteiro N. Apresente " +
+                "o quadrado de cada um dos valores pares, de 1 até N, inclusive N, se for o caso.");
+
+            int numero = int.Parse(Console.ReadLine());
+
+            if(numero <= 1)
+                Console.WriteLine("Não é possível calcular números menores que 1");
+            else
+            {
+                for(int i = 1; i <= numero; i++)
+                {
+                    if(i % 2 == 0)
+                    {
+                        Console.WriteLine($"{i} ^ 2 = {Math.Pow(i, 2)}");
+                    }
+                }
+            }
+        
+        }
+    }
+}
