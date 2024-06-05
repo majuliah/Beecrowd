a828d1a9729c72684bdca628e15ea3cfd232bc0c
Majulia <majulialcarvalho@gmail.com>
2024-06-05 19:19:30 -0300

    exercicio 1173 resolvido, aceito no beecrowd

--- Iniciante/Exercice1172/Program.cs
+++ Iniciante/Exercice1172/Program.cs
@@ -8,9 +8,24 @@ namespace Exercice1172
         static void Main(string[] args)
         {
             //Exercice1172
-            //Exercice1172();
+            //Exercice1172();            
+            
+            //Exercice1173
+            Exercice1173();
+        }
+        static void Exercice1173()
+        {
+            int[] vetorDobro = new int[10];
+            int numeroInicial = int.Parse(ReadLine());
 
+            for (int i = 0; i < vetorDobro.Length; i++)
+            {
+                vetorDobro[i] = numeroInicial;
+                numeroInicial *= 2;
+            }
 
+            for (int i = 0; i < vetorDobro.Length; i++)
+                WriteLine($"N[{i}] = {vetorDobro[i]}");
         }
 
         static void Exercice1172()
