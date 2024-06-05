19783e3cd2611bacbd734eafe18f4a79697d6af9
Majulia <majulialcarvalho@gmail.com>
2024-06-05 19:44:57 -0300

    exercicio 1174 resolvido, aceito no beecrowd

--- Iniciante/Exercice1172/Program.cs
+++ Iniciante/Exercice1172/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using static System.Console;
+using System.Globalization;
 
 namespace Exercice1172
 {
@@ -11,7 +12,29 @@ namespace Exercice1172
             //Exercice1172();            
             
             //Exercice1173
-            Exercice1173();
+            //Exercice1173();
+            
+            //Exercice1174
+            Exercice1174();
+        }
+
+        static void Exercice1174()
+        {
+            double[] vetorNumeros = new double[100];
+            double numeros;
+
+            for (int i = 0; i < vetorNumeros.Length; i++)
+            {
+                numeros = double.Parse(ReadLine(), CultureInfo.InvariantCulture);
+                vetorNumeros[i] = numeros;
+            }
+
+            for (int i = 0; i < vetorNumeros.Length; i++)
+            {
+                if (vetorNumeros[i] <= 10)
+                    WriteLine($"A[{i}] = {vetorNumeros[i].ToString("F1", CultureInfo.InvariantCulture)}");
+            }
+
         }
         static void Exercice1173()
         {
